b68120778e608019d979c57b02fca6da762db2c9
James Bradley <jbradley@blvd.co>
2020-09-15 17:46:21 +0100

    DSL exploration checkpoint

--- lib/neo/query.ex
+++ lib/neo/query.ex
@@ -1,4 +1,7 @@
 defmodule Neo.Query do
+  require Amnesia
+  require Amnesia.Helper
+
   defmacro fact(head_rule, do: expression) do
     IO.inspect(head_rule)
     IO.inspect(expression)
@@ -16,6 +19,44 @@ defmodule Neo.Query do
       s = :datalog.q(e, [{:a, 1}, {:b, 2}, {:c, 3}])
       IO.inspect(s)
       # assert ids == [1]
+    end
+  end
+
+  def run do
+    Amnesia.transaction do
+      q =
+        :datalog.p('?- h(_, _).
+                      f(s, p, o).
+                      h(s, title) :-
+                        f(s, "year", 1987),
+                        f(s, "title", title).
+                      ')
+        |> IO.inspect()
+
+      q = [
+        {:goal, :i, [:_, :_]},
+        {:source, Neo.Event, [:entity, :attribute, :value]},
+        {:horn, :h, [:entity, :title],
+         [
+           %{@: Neo.Event, _: [:entity, Movies.Movie.Year, 1987]},
+           %{@: Neo.Event, _: [:entity, Movies.Movie.Title, :title]}
+         ]},
+        {:horn, :i, [:entity, :title],
+         [
+           %{@: Neo.Event, _: [:entity, Movies.Movie.Year, 1986]},
+           %{@: Neo.Event, _: [:entity, Movies.Movie.Title, :title]}
+         ]}
+      ]
+
+      events =
+        for {:ok, event} <- Neo.Repo.Database.Event.keys() |> Enum.map(&Neo.Repo.get/1),
+            do: {event.entity, event.attribute, event.value}
+
+      events |> IO.inspect()
+      IO.inspect(q)
+      e = :datalog.c(:datalog_list, q)
+      s = :datalog.q(e, events)
+      IO.inspect(s)
     end
   end
 end
