4c5370be817a4915e02ece216347988079a34ace
James Bradley <jbradley@blvd.co>
2020-09-28 18:47:39 +0100

    Some sort of DSL?

--- lib/neo/fact.ex
+++ lib/neo/fact.ex
@@ -2,26 +2,15 @@ defmodule Neo.Fact do
   require Amnesia
   require Amnesia.Helper
 
-  defmacro __using__(source: source, select: select) do
+  defmacro __using__(source: source) do
     quote do
       import Neo.Fact
 
-      @goal :h
+      @goal :goal
+      @query []
+      @source unquote(source)
 
-      def query(),
-        do:
-          [
-            Neo.Fact.goal(@goal, unquote(select)),
-            Neo.Fact.source(unquote(source)),
-            {:horn, @goal, [:id, :title, :director],
-             [
-               %{@: Neo.Event, _: [:id, to_string(Movies.Movie.Year), 1987]},
-               %{@: Neo.Event, _: [:id, to_string(Movies.Movie.Title), :title]},
-               %{@: Neo.Event, _: [:id, to_string(Movies.Movie.Director), :did]},
-               %{@: Neo.Event, _: [:did, to_string(Movies.Actor.Name), :director]}
-             ]}
-          ]
-          |> IO.inspect()
+      @before_compile Neo.Fact
     end
   end
 
@@ -31,14 +20,36 @@ defmodule Neo.Fact do
 
   def source(module), do: {:source, module, [:e, :a, :v]}
 
-  defmacro fact(do: expression) do
-    IO.inspect(expression)
+  def line({:where, _, [{:__aliases__, _, _} = a, e, v]}) do
+    {:%{}, [],
+     [
+       @: {:__aliases__, [alias: false], [:Neo, :Event]},
+       _: [
+         Neo.Fact.var(e),
+         {:to_string, [context: Elixir, import: Kernel], [a]},
+         Neo.Fact.var(v)
+       ]
+     ]}
+  end
 
+  def var({:@, _, [{v, _, _}]}), do: v
+  def var(x), do: x
+
+  defmacro select(vars, do: {:__block__, [], lines}) do
     quote do
+      @query [Neo.Fact.goal(@goal, unquote(vars)) | @query]
+      @query [Neo.Fact.source(@source) | @query]
+
+      @query [
+        {:horn, @goal, unquote(vars), unquote(Enum.map(lines, &line/1))}
+        | @query
+      ]
     end
   end
 
-  defmacro where(entity, attribute, value) do
-    IO.inspect({entity, attribute, value})
+  defmacro __before_compile__(_env) do
+    quote do
+      def query(), do: @query |> Enum.reverse() |> IO.inspect(label: "query")
+    end
   end
 end
--- test/neo/fact_test.exs
+++ test/neo/fact_test.exs
@@ -1,43 +1,45 @@
+defmodule SomeFact do
+  use Neo.Fact, source: Neo.Event
+
+  select [:id, :title, :director, :year] do
+    where(Movies.Movie.Year, @id, @year)
+    where(Movies.Movie.Title, @id, @title)
+    where(Movies.Movie.Director, @id, @did)
+    where(Movies.Actor.Name, @did, @director)
+  end
+end
+
 defmodule Neo.FactTest do
   use ExUnit.Case
 
-  test "successfully queries a raw fact" do
-    defmodule RawFact do
-      use Neo.Fact, source: Neo.Event, select: [:id, :title, :director]
-
-      @raw [
-        {:goal, :h, [:_, :_, :_]},
-        {:source, Neo.Event, [:e, :a, :v]},
-        {:horn, :h, [:id, :title, :director],
-         [
-           %{@: Neo.Event, _: [:id, to_string(Movies.Movie.Year), 1987]},
-           %{@: Neo.Event, _: [:id, to_string(Movies.Movie.Title), :title]},
-           %{@: Neo.Event, _: [:id, to_string(Movies.Movie.Director), :did]},
-           %{@: Neo.Event, _: [:did, to_string(Movies.Actor.Name), :director]}
-         ]}
-      ]
-    end
-
-    assert Neo.Repo.all(RawFact) == [
-             ["urn:movie:202", "Predator", "John McTiernan"],
-             ["urn:movie:203", "Lethal Weapon", "Richard Donner"],
-             ["urn:movie:204", "RoboCop", "Paul Verhoeven"]
+  test "succesfully queries a fact" do
+    assert Neo.Repo.query(SomeFact) == [
+             ["urn:movie:200", "The Terminator", "James Cameron", 1984],
+             ["urn:movie:201", "First Blood", "Ted Kotcheff", 1982],
+             ["urn:movie:202", "Predator", "John McTiernan", 1987],
+             ["urn:movie:203", "Lethal Weapon", "Richard Donner", 1987],
+             ["urn:movie:204", "RoboCop", "Paul Verhoeven", 1987],
+             ["urn:movie:205", "Commando", "Mark L. Lester", 1985],
+             ["urn:movie:206", "Die Hard", "John McTiernan", 1988],
+             ["urn:movie:207", "Terminator 2: Judgment Day", "James Cameron", 1991],
+             ["urn:movie:208", "Terminator 3: Rise of the Machines", "Jonathan Mostow", 2003],
+             ["urn:movie:209", "Rambo: First Blood Part II", "George P. Cosmatos", 1985],
+             ["urn:movie:210", "Rambo III", "Peter MacDonald", 1988],
+             ["urn:movie:211", "Predator 2", "Stephen Hopkins", 1990],
+             ["urn:movie:212", "Lethal Weapon 2", "Richard Donner", 1989],
+             ["urn:movie:213", "Lethal Weapon 3", "Richard Donner", 1992],
+             ["urn:movie:214", "Alien", "Ridley Scott", 1979],
+             ["urn:movie:215", "Aliens", "James Cameron", 1986],
+             ["urn:movie:216", "Mad Max", "George Miller", 1979],
+             ["urn:movie:217", "Mad Max 2", "George Miller", 1981],
+             ["urn:movie:218", "Mad Max Beyond Thunderdome", "George Miller", 1985],
+             ["urn:movie:218", "Mad Max Beyond Thunderdome", "George Ogilvie", 1985],
+             ["urn:movie:219", "Braveheart", "Mel Gibson", 1995]
            ]
   end
 
-  test "succesfully queries a macro-d fact" do
-    defmodule SomeFact do
-      use Neo.Fact, source: Neo.Event, select: [:id, :title, :director]
-
-      fact do
-        where(Movies.Movie.Year, @id, 1987)
-        where(Movies.Movie.Title, @id, @title)
-        where(Movies.Movie.Director, @id, @did)
-        where(Movies.Actor.Name, @did, @director)
-      end
-    end
-
-    assert Neo.Repo.all(SomeFact) == [
+  test "successfully constrains a fact" do
+    assert Neo.Repo.query(SomeFact) == [
              ["urn:movie:202", "Predator", "John McTiernan"],
              ["urn:movie:203", "Lethal Weapon", "Richard Donner"],
              ["urn:movie:204", "RoboCop", "Paul Verhoeven"]
